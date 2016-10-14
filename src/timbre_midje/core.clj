c774a30e46743acacac1fe00a74768d54baba489
Stephane Bruckert <contact@stephanebruckert.com>
2016-10-14 19:14:46 +0100

    Keep essential

--- src/timbre_midje/core.clj
+++ src/timbre_midje/core.clj
@@ -2,12 +2,7 @@
   ;; Clojure
   (:require [taoensso.timbre :as timbre]))
 
-;; (defn my-output-fn [data]
-;;   (println "my-output-fn was called")
-;;   (timbre/default-output-fn data))
-
 (defn init [level]
-  (timbre/merge-config! {:output-fn my-output-fn})
   (timbre/set-level! level)
   (timbre/info "Will be printed")
   (timbre/debug "Won't be printed"))
--- test/timbre_midje/core_test.clj
+++ test/timbre_midje/core_test.clj
@@ -4,21 +4,7 @@
             [midje.sweet :refer :all]))
 
 (fact "print info but not debug"
-      (core/init :info) => nil
-      (provided
-       (timbre/default-output-fn (as-checker (fn [opts]
-                                        (= (-> opts :config :level)
-                                           :info))))
-       => anything :times 1)
-      (provided
-       (timbre/default-output-fn (as-checker (fn [opts]
-                                        (= (-> opts :config :level)
-                                           :debug))))
-       => anything :times 0))
-
-(fact "print Will be printed, not Won't be printed"
-      (core/init :info) => nil
-      (provided
-       (#'timbre/vargs->margs anything anything ["Will be printed"]) => {} :times 1)
-      (provided
-       (#'timbre/vargs->margs anything anything ["Won't be printed"]) => {} :times 0) )
+  (core/init :info) => nil
+  (provided
+    (#'timbre/vargs->margs anything anything ["Will be printed"]) => {} :times 1
+    (#'timbre/vargs->margs anything anything ["Won't be printed"]) => {} :times 0))
